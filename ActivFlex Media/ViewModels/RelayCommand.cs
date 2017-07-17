f9c740bc550b3541dd57e93fd279942ae7fe29eb
henrik-peters <peters-henrik@outlook.de>
2017-07-17 19:16:27 +0200

    relay command extension for a parameter

--- ActivFlex Media/ViewModels/RelayCommand.cs
+++ ActivFlex Media/ViewModels/RelayCommand.cs
@@ -32,10 +32,11 @@ namespace ActivFlex.ViewModels
 
         /// <summary>
         /// Is raised when <see cref="canExecute(object)"/> has changed.
+        /// The CommandManager is not connected to save performance.
         /// </summary>
         public event EventHandler CanExecuteChanged {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add { /*CommandManager.RequerySuggested += value;*/ }
+            remove { /*CommandManager.RequerySuggested -= value;*/ }
         }
 
         /// <summary>
@@ -68,6 +69,59 @@ namespace ActivFlex.ViewModels
         public void Execute(object parameter)
         {
             _targetMethod?.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// This class can be used to link a command property to
+    /// an executable action in the viewModel. This allows a
+    /// simple forward execution. It uses one parameter.
+    /// </summary>
+    /// <typeparam name="T">Type of the the parameter</typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _targetMethod;
+        private readonly Predicate<T> _canExecute;
+
+        /// <summary>
+        /// Is raised when <see cref="canExecute(object)"/> has changed.
+        /// The CommandManager is not connected to save performance.
+        /// </summary>
+        public event EventHandler CanExecuteChanged {
+            add { /*CommandManager.RequerySuggested += value;*/ }
+            remove { /*CommandManager.RequerySuggested -= value;*/ }
+        }
+
+        /// <summary>
+        /// Create a new relay command that will run the passed
+        /// action when <see cref="Execute(object)"/> is called.
+        /// </summary>
+        /// <param name="execute">Action for relaying to</param>
+        /// <param name="canExecute">Predicate to call for canExecute</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            this._targetMethod = execute;
+            this._canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Check if this command can be executed by the
+        /// canExecute function (passed by the constructor).
+        /// </summary>
+        /// <param name="parameter">Has to be of the type T</param>
+        /// <returns>True when the command can execute</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute.Invoke((T)parameter);
+        }
+
+        /// <summary>
+        /// Execute the action passed by the constructor.
+        /// </summary>
+        /// <param name="parameter">Has to be of the type T</param>
+        public void Execute(object parameter)
+        {
+            _targetMethod?.Invoke((T)parameter);
         }
     }
 }
