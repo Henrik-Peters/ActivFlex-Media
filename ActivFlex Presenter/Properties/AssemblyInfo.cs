d6f8688bd4594194996b28c28cc2802a4fa515a6
henrik-peters <peters-henrik@outlook.de>
2017-07-24 20:58:08 +0200

    new presentation control library

---
+++ ActivFlex Presenter/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("ActivFlex Presenter")]
+[assembly: AssemblyDescription("Presentation controls module for ActivFlex")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ActivFlex Presenter")]
+[assembly: AssemblyCopyright("Copyright © 2017 Henrik Peters")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en")]
