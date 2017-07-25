21eb287124d5bd6dc853d7229ee438b1ef7d4515
henrik-peters <peters-henrik@outlook.de>
2017-07-25 02:37:08 +0200

    implementation of the image presenter

--- ActivFlex Presenter/Presentation/ImagePresenter.xaml.cs
+++ ActivFlex Presenter/Presentation/ImagePresenter.xaml.cs
@@ -15,20 +15,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see<http://www.gnu.org/licenses/>.
 #endregion
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ActivFlex.Presentation
 {
@@ -37,9 +27,100 @@ namespace ActivFlex.Presentation
     /// </summary>
     public partial class ImagePresenter : UserControl
     {
+        //Points for the drag translate with the mouse
+        private Point mouseStart;
+        private Point translateOrigin;
+
+        //Zoom constants
+        private const double zoomSpeed = .2D;
+
+        #region DependencyProperties
+
+        /// <summary>
+        /// Identifies the PathProperty dependency property
+        /// </summary>
+        public static readonly DependencyProperty PathProperty = DependencyProperty.Register(
+            "Image", typeof(ImageSource), typeof(ImagePresenter), new FrameworkPropertyMetadata(Image_PropertyChanged));
+
+        /// <summary>
+        /// Gets or sets the image data source 
+        /// used to present the current image.
+        /// </summary>
+        public ImageSource Image {
+            get => (ImageSource)this.GetValue(PathProperty);
+            set => this.SetValue(PathProperty, value);
+        }
+
+        #endregion
+
         public ImagePresenter()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Reset all image rendering transformations
+        /// back to their default values. Will be called 
+        /// automatically when the image has changed.
+        /// </summary>
+        public void ResetRenderTransform()
+        {
+            tScale.ScaleX = 1.0;
+            tScale.ScaleY = 1.0;
+            tTranslate.X = 0;
+            tTranslate.Y = 0;
+            tRotate.Angle = 0;
+            tSkew.AngleX = 0;
+            tSkew.AngleY = 0;
+        }
+
+        private static void Image_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var presenter = (ImagePresenter)d;
+            presenter.ImgDisplay.Source = presenter.Image;
+
+            //Reset the rendering options
+            presenter.ResetRenderTransform();
+            presenter.ImgDisplay_SizeChanged(presenter, null);
+        }
+
+        private void UserControl_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double deltaZoom = e.Delta > 0 ? zoomSpeed : -zoomSpeed;
+            tScale.ScaleX += deltaZoom;
+            tScale.ScaleY += deltaZoom;
+        }
+
+        private void ImgDisplay_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //Use the center of the image as scaling transform center
+            tScale.CenterX = ImgDisplay.ActualWidth / 2;
+            tScale.CenterY = ImgDisplay.ActualHeight / 2;
+        }
+
+        private void ImgDisplay_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Handle mouse events only for the image during a drag
+            ImgDisplay.CaptureMouse();
+
+            mouseStart = Mouse.GetPosition(GridPresenter);
+            translateOrigin = new Point(tTranslate.X, tTranslate.Y);
+        }
+
+        private void ImgDisplay_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ImgDisplay.ReleaseMouseCapture();
+        }
+
+        private void ImgDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!ImgDisplay.IsMouseCaptured)
+                return;
+
+            //Move the image based on the mouse drag vector
+            Vector dragVector = mouseStart - Mouse.GetPosition(GridPresenter);
+            tTranslate.X = translateOrigin.X - dragVector.X;
+            tTranslate.Y = translateOrigin.Y - dragVector.Y;
         }
     }
 }
